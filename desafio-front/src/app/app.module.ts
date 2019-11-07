ee78315f363520c8cecbb6651e9a9e8e1716d44a
Giovanni Biffi <gantesbr@gmail.com>
2019-11-07 12:01:43 -0400

    Formatação Layout Tela Inicial
    Html padrão para formatação da tela inicial antes de componentizar.

--- desafio-front/src/app/app.module.ts
+++ desafio-front/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { CardModule } from './shared/card/card.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CardModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
