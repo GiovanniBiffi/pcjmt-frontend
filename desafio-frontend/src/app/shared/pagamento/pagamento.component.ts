8b1d7f24e6c26851e2925ed4e1f5a40a2d225f2c
Giovanni Biffi <gantesbr@gmail.com>
2019-11-08 17:29:07 -0400

    Dinamica tela de pagamento
    funcionamento da tela de pagamento.

--- desafio-frontend/src/app/shared/pagamento/pagamento.component.ts
+++ desafio-frontend/src/app/shared/pagamento/pagamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-pagamento',
@@ -8,4 +9,43 @@ import { Component, Input } from '@angular/core';
 
 export class PagamentoComponent{
      
+    @Input() ispago:boolean = false;
+    cartaoForm : FormGroup;
+
+     constructor(
+        private formBuilder: FormBuilder
+    ){}
+
+    ngOnInit() : void{
+    this.cartaoForm = this.formBuilder.group({
+        cartaoNumero:['',
+            [
+              Validators.minLength(16),
+              Validators.maxLength(16)
+            ]
+        ],
+        cartaoNome:['',
+            [                     
+              Validators.minLength(5),
+              Validators.maxLength(100)
+            ]
+        ],
+        cartaoValidade:['', 
+            [                       
+                Validators.minLength(5),
+                Validators.maxLength(5)
+            ]
+        ],
+        cartaoCodSeg:['',
+            [
+                Validators.minLength(3),
+                Validators.maxLength(3)
+            ]
+        ]
+    });
+}
+
+     enviaCartao(){
+         this.ispago = true;         
+     }
 }
--- desafio-frontend/src/app/shared/pagamento/pagamento.module.ts
+++ desafio-frontend/src/app/shared/pagamento/pagamento.module.ts
@@ -2,13 +2,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagamentoComponent } from './pagamento.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
     declarations: [PagamentoComponent],
     exports: [PagamentoComponent],
-    imports:[CommonModule]
+    imports:[
+        FormsModule,
+        ReactiveFormsModule, 
+        CommonModule
+    ]
 })
 export class PagamentoModule{
 
